18e6169ad8a7dcb0586f3231bc74fdec1f75ae69
André Sugai <guitarfreaks@gmail.com>
2023-02-18 18:11:24 -0300

    feat: somando valor do dado com o valor de score do jogador

--- script.js
+++ script.js
@@ -4,6 +4,8 @@
 // dua maneiras de selecionar id's
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
 const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
@@ -29,4 +31,11 @@ btnRoll.addEventListener('click', function () {
   diceEl.src = `dice-${dice}.png`;
 
   // 3. Check for rolled 1: if true, switch to next player
+  if (dice !== 1) {
+    //Add dice to current score
+    currentScore += dice;
+    current0El.textContent = currentScore; // CHANGE LATER
+  } else {
+    // Switch to next player
+  }
 });
