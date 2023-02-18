ab024ad11e5702f80e1fabc91338e2c493f1e7cb
André Sugai <guitarfreaks@gmail.com>
2023-02-18 17:57:46 -0300

    feat: botão roll ativa número aleatório e chama imagem do dado com número correspondente

--- script.js
+++ script.js
@@ -14,3 +14,19 @@ const btnHold = document.querySelector('.btn--hold');
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
+
+let currentScore = 0;
+
+// Rolling dice functionality
+
+btnRoll.addEventListener('click', function () {
+  // 1. Generating a randon dice roll
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  console.log(diceEl);
+
+  // 2. Display dice
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${dice}.png`;
+
+  // 3. Check for rolled 1: if true, switch to next player
+});
