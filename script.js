7897840c9f4f48f2381e8153ecf5fed8906b6ac3
André Sugai <guitarfreaks@gmail.com>
2023-02-18 17:20:08 -0300

    feat: botões de new, roll e hold capturados

--- script.js
+++ script.js
@@ -5,6 +5,9 @@
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
 const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
 
 //Starting conditions
 //Apesar de serem number o javascript converte para string quando mostra na página
